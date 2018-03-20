6405f2af807897e8656309aa0fcf423136422316
CJ Rhone and Jim Palowski <cj@>
2018-03-20 14:29:25 -0700

    Add descriptions and functionality to each card

---
+++ js/scripts.js
@@ -0,0 +1,80 @@
+$(document).ready(function() {
+$(".view").click(function() {
+  $("#Java").slideToggle();
+});
+$(".Operate").click(function(){
+  $("#Operate").slideToggle();
+});
+$(".Vary").click(function(){
+  $("#Variable").slideToggle();
+});
+
+$(".Vary").click(function(){
+  $("#Variable").slideToggle();
+
+});
+$(".VNC").click(function(){
+  $("#VNC").slideToggle();
+});
+
+$(".Func").click(function(){
+  $("#Function").slideToggle();
+});
+
+$(".Method").click(function(){
+  $("#Method").slideToggle();
+});
+
+$(".Argument").click(function(){
+  $("#Argument").slideToggle();
+});
+
+$(".Para").click(function(){
+  $("#Parameter").slideToggle();
+});
+
+$(".Return").click(function(){
+  $("#Return").slideToggle();
+});
+
+$(".Chain").click(function(){
+  $("#Chaining").slideToggle();
+});
+
+$(".String").click(function(){
+  $("#String").slideToggle();
+});
+
+$(".Bool").click(function(){
+  $("#Bool").slideToggle();
+});
+
+$(".UNF").click(function(){
+  $("#Undefined").slideToggle();
+});
+
+$(".NaN").click(function(){
+  $("#NaN").slideToggle();
+});
+
+$(".Escape").click(function(){
+  $("#Escape").slideToggle();
+});
+
+$(".Alert").click(function(){
+  $("#Alert").slideToggle();
+});
+
+$(".Comment").click(function(){
+  $("#Comment").slideToggle();
+});
+
+$(".JQuery").click(function(){
+  $("#JQuery").slideToggle();
+});
+
+
+$(".Attribute").click(function(){
+  $("#Attribute").slideToggle();
+});
+});
